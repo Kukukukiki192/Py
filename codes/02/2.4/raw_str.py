d1e3520e71ce6646af7b52c610303a2887d242db
Kukukukiki192 <60028604+Kukukukiki192@users.noreply.github.com>
2022-12-01 23:41:53 +0800

    0

---
+++ codes/02/2.4/raw_str.py
@@ -0,0 +1,25 @@
+# coding: utf-8
+#########################################################################
+# 网站: <a href="http://www.crazyit.org">疯狂Java联盟</a>               #
+# author yeeku.H.lee kongyeeku@163.com                                  #
+#                                                                       #
+# version 1.0                                                           #
+#                                                                       #
+# Copyright (C), 2001-2018, yeeku.H.Lee                                 #
+#                                                                       #
+# This program is protected by copyright laws.                          #
+#                                                                       #
+# Program Name:                                                         #
+#                                                                       #
+# <br>Date:                                                             #
+#########################################################################
+s1 = r'G:\publish\codes\02\2.4'
+print(s1)
+# 原始字符串包含的引号，同样需要转义
+s2 = r'"Let\'s go", said Charlie'
+print(s2)
+s3 = r'Good Morning' '\\'
+print(s3)
+
+s3 = r'Good Morning\\' '\\'  # 前2个\保留 后2个:前者的作用是转义后者只显示后者
+print(s3)
